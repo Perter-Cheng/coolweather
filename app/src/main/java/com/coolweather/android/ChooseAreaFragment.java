9d89fa930dfbd77dd8f5f14b1d4cf1fb30e3713f
Tony <tony@gmail.com>
2019-07-30 11:05:23 +0800

    新增切换城市和手动更新天气的功能。

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -101,10 +101,21 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 }else if (currentLevel == LEVEL_County){
                     String weatherId = countyList.get(position).getWeatherId();
-                    Intent intent = new Intent(getActivity(),WeatherActivity.class);
-                    intent.putExtra("weather_id",weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    Log.d(TAG, "Activity is MainActivity == "+
+                            (getActivity() instanceof MainActivity));
+                    Log.d(TAG, "Activity is WeatherActivity ==  "+
+                            (getActivity() instanceof WeatherActivity));
+                    if (getActivity() instanceof MainActivity){
+                        Intent intent = new Intent(getActivity(),WeatherActivity.class);
+                        intent.putExtra("weather_id",weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }else if (getActivity() instanceof WeatherActivity){
+                        WeatherActivity activity = (WeatherActivity)getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
                 }
             }
         });
--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -5,16 +5,19 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SharedPreferences prefs = PreferenceManager.
                 getDefaultSharedPreferences(this);
+        Log.d(TAG, "prefs.String = " + prefs.getString("weather",null));
         if (prefs.getString("weather",null) != null){
+            Log.d(TAG, "start WeatherActivity");
             Intent intent = new Intent(this,WeatherActivity.class);
             startActivity(intent);
             finish();
